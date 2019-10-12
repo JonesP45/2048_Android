0113f2db01857b0561c55bf8e7a6718f5bee51f0
Jean-Pacome Delmas <jean-pacome.delmas@etu.univ-orleans.fr>
2019-10-12 10:09:38 +0200

    Création fonction vide

--- app/src/main/java/fr/univ_orleans/a2048/ModeleJeu.java
+++ app/src/main/java/fr/univ_orleans/a2048/ModeleJeu.java
@@ -1,4 +1,47 @@
 package fr.univ_orleans.a2048;
 
+import android.util.Pair;
+
+import java.util.ArrayList;
+
 public class ModeleJeu {
+
+    private int[][] grille;
+    private int score;
+    private ArrayList<Pair<Integer, Integer>> liste;
+
+    public ModeleJeu() {
+        grille = new int[4][4];
+        score = 0;
+        liste = new ArrayList<>();
+    }
+
+    public void moveDroite() {
+
+    }
+
+    public void moveGauche() {
+
+    }
+
+    public void moveHaut() {
+
+    }
+
+    public void moveBas() {
+
+    }
+
+    public void modifScore() {
+
+    }
+
+    public boolean verifFinJeu() {
+        return false;
+    }
+
+    public void jeu() {
+
+    }
+
 }
