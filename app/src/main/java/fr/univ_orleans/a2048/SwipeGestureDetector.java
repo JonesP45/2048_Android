f07f0a5e59070e2afcb593fed92f0ccdc97a72d0
Jean-Pacome Delmas <jean-pacome.delmas@etu.univ-orleans.fr>
2019-10-26 16:41:23 +0200

    gestion des swipes

---
+++ app/src/main/java/fr/univ_orleans/a2048/SwipeGestureDetector.java
@@ -0,0 +1,46 @@
+package fr.univ_orleans.a2048;
+
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class SwipeGestureDetector extends GestureDetector {
+
+    private final static int DELTA_MIN = 50;
+
+    public static enum SwipeDirection {
+        LEFT_TO_RIGHT, RIGHT_TO_LEFT, TOP_TO_BOTTOM, BOTTOM_TO_TOP
+    }
+
+    public SwipeGestureDetector(final MainActivity context) {
+        super(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            //gère les swipes
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                Log.i("DEBUG", e1 + " - " + e2);
+
+                float deltaX = e2.getX() - e1.getX();
+                float deltaY = e2.getY() - e1.getY();
+
+                if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                    if (Math.abs(deltaX) >= DELTA_MIN) {
+                        if (deltaX < 0)
+                            context.onSwipe(SwipeDirection.RIGHT_TO_LEFT);
+                        else
+                            context.onSwipe(SwipeDirection.LEFT_TO_RIGHT);
+                        return true;
+                    }
+                } else {
+                    if (Math.abs(deltaY) >= DELTA_MIN) {
+                        if (deltaY < 0)
+                            context.onSwipe(SwipeDirection.BOTTOM_TO_TOP);
+                        else
+                            context.onSwipe(SwipeDirection.TOP_TO_BOTTOM);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+}
