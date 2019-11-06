72d158378133cb895ef3072f7db42664dc5dce77
Lionel Nouboussi <lionel.nouboussi@gmail.com>
2019-11-06 14:56:38 +0100

    ajout de la grille dans le xml fragment_jeu

---
+++ app/src/main/java/fr/univ_orleans/a2048/Layouts/SquareLayout.java
@@ -0,0 +1,64 @@
+package fr.univ_orleans.a2048.Layouts;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class SquareLayout extends LinearLayout {
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        int size = Math.min(getMeasuredHeight(),getMeasuredWidth());
+
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(size,MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(size,MeasureSpec.EXACTLY));
+
+        init();
+    }
+
+    private void init() {
+        this.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+
+                if (getWidth() != getHeight()) {
+                    //Get the smaller dimension of height and width
+                    int squareSize = Math.min(getWidth(), getHeight());
+
+                    //Set the dimensions to a Square
+                    ViewGroup.LayoutParams lp = getLayoutParams();
+                    lp.width = squareSize;
+                    lp.height = squareSize;
+                    requestLayout();
+                    return false;
+                }
+                return true;
+
+            }
+        });
+    }
+}
